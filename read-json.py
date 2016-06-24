5eb1215babc80bde1c58cff3d3505422ff3520e0
James Kingsmill <james.kingsmill@gmail.com>
2016-06-24 18:04:35 +1000

    Write output to yaml

--- read-json.py
+++ read-json.py
@@ -1,11 +1,18 @@
-import json
+import json, yaml
 from pprint import pprint
 
 with open('sample.json') as data_file:
     data = json.load(data_file)
+
+yml = yaml.safe_dump(data)
+
+f = open('out.yaml', 'w')
+f.write(yml)
+f.close()
 
 pprint(data)
 
 print("---")
 
 print(data["maps"][0]["id"])
+print(data["masks"]["id"])
